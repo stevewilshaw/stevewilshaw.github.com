9a83da1b5cad1a81764bf86924bb4b32f6c058ec
Steve Wilshaw <gyokushin@gmail.com>
2012-08-15 23:59:41 +0100

    Added Portfolio Page

--- assets/javascripts/portfolio.js
+++ assets/javascripts/portfolio.js
@@ -1,107 +1,104 @@
-var portfolio = {
+var portfolio = (function () {  
 
-	init: function() {
-		
-		// Delay activation each tile
-		for(var i = 0; i <8; i++) {
-			setTimeout((function(d) { 
-
-				return function() {
-					$(".tile").eq(d).addClass("show");
-				}
-				
-			})(i),120 + i * 100);
-		}
-
-		// Bind Tile clicks
-		$("a.tile").click(function(event) {
-			event.preventDefault();
-			event.stopPropagation();
-			var id = $(this).attr("href");
-			portfolio.showProject(id);					
-		});
-
-		// Bind Next & Previous Button clicks
-		$(".next").click(function(event) {
-			event.stopPropagation();
-			portfolio.nextProject();
-		});
-
-		$(".prev").click(function(event) {
-			event.stopPropagation();
-			portfolio.previousProject();			
-		});
-
-		// Reset Everything on document click
-		$(document).click(function() {
-			portfolio.reset();
-		});
-
-		//Bind Keys
-		$(document).keyup(function(e){
-            
-            if(e.which == 27){
-            	portfolio.reset();
-            }
-
-            if (e.keyCode == 13) {
-            	var id = "#pr1";
-            	if ($(".projects").hasClass("projects-active"))
-            		portfolio.reset();
-            	else
-            		portfolio.showProject(id);	
-            }
-
-            if (e.keyCode == 37) {
-            	portfolio.previousProject();
-            }
-
-            if (e.keyCode == 39) {
-            	portfolio.nextProject();
-            }
-
-         });
-	},
-
-	showProject: function(id) {
+	function showProject(id) {
 		$(".projects").addClass("projects-active");
 		$(id).addClass("project-active");
 		$(".tiles").addClass("inactive");
 		$(id).nextAll(".project").addClass("hide-right");	
 		$(id).prevAll(".project").addClass("hide-left");
-	},
+	}
 
-	nextProject: function() {
+	function nextProject() {
 		next = $(".project-active").next();
 		if (next.length) {
 			$(".project-active").removeClass("project-active").addClass("hide-left");
 			next.removeClass("hide-right").addClass("project-active");
 		}
-	},
+	}
 
-	previousProject: function() {
+	function previousProject() {
 		prev = $(".project-active").prev();
 		if (prev.length) {
 			$(".project-active").removeClass("project-active").addClass("hide-right");;
 			prev.removeClass("hide-left").addClass("project-active");
 		}
-	},
+	}
 
-	reset: function() {
+	function reset() {
 		$(".projects").removeClass("projects-active");
 		$(".project").removeClass("project-active hide-left hide-right");
 		$(".tiles").removeClass("inactive");
 	}
 
-};
+	// Start on window.load after all the images have loaded.
 
+	$(window).load(function() {
+
+		// Only initialise if browser support transitions. An alternate style for the 
+		// portfolio us defined using modernizr's css classes.
+
+		if (Modernizr.opacity && Modernizr.csstransforms) {
+
+			// Delay activation each tile
+			for(var i = 0; i <8; i++) {
+				setTimeout((function(d) { 
+
+					return function() {
+						$(".tile").eq(d).addClass("show");
+					}
+					
+				})(i),120 + i * 100);
+			}
+
+			// Bind Tile clicks
+			$("a.tile").click(function(event) {
+				event.preventDefault();
+				event.stopPropagation();
+				var id = $(this).attr("href");
+				showProject(id);					
+			});
+
+			// Bind Next & Previous Button clicks
+			$(".next").click(function(event) {
+				event.stopPropagation();
+				nextProject();
+			});
+
+			$(".prev").click(function(event) {
+				event.stopPropagation();
+				previousProject();			
+			});
+
+			// Reset Everything on document click
+			$(document).click(function() {
+				reset();
+			});
+
+			//Bind Keys
+			$(document).keyup(function(e){
+	            
+	            if(e.which == 27){
+	            	reset();
+	            }
+
+	            if (e.keyCode == 13) {
+	            	var id = "#pr1";
+	            	if ($(".projects").hasClass("projects-active"))
+	            		reset();
+	            	else
+	            		showProject(id);	
+	            }
+
+	            if (e.keyCode == 37) {
+	            	previousProject();
+	            }
+
+	            if (e.keyCode == 39) {
+	            	nextProject();
+	            }
+
+	        });
+		}
+	});
 	
-$(window).load(function() {
-
-	// Only initialise if browser support transitions. An alternate style for the 
-	// portfolio us defined using modernizr's css classes.
-
-	if (Modernizr.opacity && Modernizr.csstransforms) {
-		portfolio.init();
-	}
-});
+})();
